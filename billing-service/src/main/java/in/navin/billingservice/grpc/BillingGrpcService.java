c7cc325d8040af2afd6a7b05ef65c845f6104522
Navin Sahu <sahunavin015@gmail.com>
2026-07-15 19:42:14 +0530

    feat(billing-service): implement gRPC billing service

---
+++ billing-service/src/main/java/in/navin/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,30 @@
+package in.navin.billingservice.grpc;
+
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+                                     StreamObserver<BillingResponse> responseObserver) {
+        log.info("createBillingAccount request received {}", billingRequest.toString());
+
+        // Business logic e.g. saves to database, performing calculations etc
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("Active")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
