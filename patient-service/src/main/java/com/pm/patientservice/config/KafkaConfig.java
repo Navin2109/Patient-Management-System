c77b5fdec087c8637121a5f728d12cb86e9cc85c
Navin Sahu <sahunavin015@gmail.com>
2026-07-23 15:53:30 +0530

    git commit -m "feat: implement Kafka producer-consumer integration with protobuf events"

---
+++ patient-service/src/main/java/com/pm/patientservice/config/KafkaConfig.java
@@ -0,0 +1,27 @@
+package com.pm.patientservice.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public KafkaTemplate<String, byte[]> kafkaTemplate() {
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class);
+
+        return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(props));
+    }
+}
